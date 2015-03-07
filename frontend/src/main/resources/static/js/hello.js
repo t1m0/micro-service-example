837c5d10714b93d072ca4de3f4ea3b73de09537a
t1m0 <timo.schoepflin@gmail.com>
2015-03-07 13:28:14 +0100

    Cleaned AngularJS

--- frontend/src/main/resources/static/js/hello.js
+++ frontend/src/main/resources/static/js/hello.js
@@ -9,15 +9,68 @@ module.config(function($routeProvider) {
 	}).otherwise('/');
 });
 /**
- * Configure debug logging
+ * Factory to access todo resource
  */
-module.config(function($logProvider){
-  $logProvider.debugEnabled(true);
+module.factory('ToDo', function($log, $http, $q){
+	return {
+		/**
+		 * Read all todos
+		 */
+		all:function(){
+			var deferred = $q.defer();
+		    var promise = deferred.promise;
+		    $http.get('resource/todo').success(function(data) {
+		    	$log.info(data);
+		    	deferred.resolve(data);
+	          })
+	          .error(function(error){
+	            deferred.reject(error);
+	          });
+		    return promise;
+		}
+	}
+});
+/**
+ * Factory to access user data
+ */
+module.factory('User', function($log, $http, $q){
+	return {
+		/**
+		 * Read all todos
+		 */
+		current:function(){
+			var deferred = $q.defer();
+		    var promise = deferred.promise;
+		    $http.get('user').success(function(data) {
+		    	$log.info(data);
+		    	deferred.resolve(data);
+	          })
+	          .error(function(error){
+	            deferred.reject(error);
+	          });
+		    return promise;
+		},
+		/**
+		 * Logout the current user
+		 */
+		logout:function(){
+			var deferred = $q.defer();
+		    var promise = deferred.promise;
+		    $http.post('logout',{}).success(function(data) {
+		    	$log.info(data);
+		    	deferred.resolve(data);
+	          })
+	          .error(function(error){
+	            deferred.reject(error);
+	          });
+		    return promise;
+		}
+	}
 });
 /**
  * Navigation controller
  */
-module.controller('navigation',function($rootScope, $scope, $http, $location, $route) {
+module.controller('navigation',function($rootScope, $scope, $location, $route, User) {
 	$scope.credentials = {};
 	$scope.tab = function(route) {
 		return $route.current && route === $route.current.controller;
@@ -25,38 +78,42 @@ module.controller('navigation',function($rootScope, $scope, $http, $location, $r
 	/**
 	 * Check authentication
 	 */
-	$http.get('user').success(function(data) {
-		if (data.name) {
-			$rootScope.authenticated = true;
-		} else {
-			$rootScope.authenticated = false;
-		}
-	}).error(function() {
-		$rootScope.authenticated = false;
-	});
+	$scope.init=function(){
+		User.current().then(
+				function(data){
+					if (data.name) {
+						$rootScope.authenticated = true;
+					} else {
+						$rootScope.authenticated = false;
+					}
+				}, function(error){
+					$rootScope.authenticated = false;
+				});
+	};
 	/**
 	 * Logout user
 	 */
 	$scope.logout = function() {
-		$http.post('logout', {}).success(function() {
-			$rootScope.authenticated = false;
-			$location.path("/");
-		}).error(function(data) {
-			console.log("Logout failed")
-			$rootScope.authenticated = false;
-		});
-	}
+		User.logout().then(
+				function(data){
+					$rootScope.authenticated = false;
+					$location.path("/");
+				}, function(error){
+					$rootScope.authenticated = false;
+				});
+	};
+	$scope.init();
 });
 /**
  * Home controller
  */
-module.controller('home', function($scope, $http, $log) {
+module.controller('home', function($scope, ToDo) {
 	$scope.todos = null;
 	$scope.init = function(){
-		$http.get('resource/todo').success(function(data) {
-			$scope.todos = data;
-			$log.info($scope.todos);
-		});
+		ToDo.all().then(
+			function(data){
+				$scope.todos=data;
+			});
 	};
 	$scope.init();
 });
